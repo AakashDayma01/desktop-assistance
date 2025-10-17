4c1e781c4efc2292931d0f871c536ce58484bdc2
AakashDayma01 <daymaaakash334@gmail.com>
2025-10-17 11:58:58 +0530

    Fix typo in divide test and update assertions

--- test_voice_assistant.py
+++ test_voice_assistant.py
@@ -10,9 +10,10 @@ class TestDesctopAssis(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(da.multiply(2, 3), 6)
         self.assertEqual(da.multiply(-1, 5), -5)
-    def test_devide(self):
-        self.assertEqual(da.devide(2, 3), 6)
-        self.assertEqual(da.devide(-1, 5), -5)
+
+    def test_divide(self):
+        self.assertAlmostEqual(da.divide(2, 3), 2/3)
+        self.assertAlmostEqual(da.divide(-1, 5), -0.2)
 
 if __name__ == "__main__":
     unittest.main()
