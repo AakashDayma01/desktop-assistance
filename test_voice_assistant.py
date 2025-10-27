aa3a4dd5a2cf5b8cacabf6395f0cfbafe1d1c9c3
AakashDayma01 <daymaaakash334@gmail.com>
2025-10-27 13:14:18 +0530

    auto commit

--- test_voice_assistant.py
+++ test_voice_assistant.py
@@ -10,6 +10,7 @@ class TestDesctopAssis(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(da.multiply(2, 3), 6)
         self.assertEqual(da.multiply(-1, 5), -5)
+
         
 
     def test_divide(self):
